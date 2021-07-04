85a360f1196c9c21a5f3a3f9003ee8a0bd2fb532
Viet-1999 <s3759306@rmit.edu.vn>
2021-07-04 15:04:17 +0700

    I solved the conflict.

--- Cat.java
+++ Cat.java
@@ -3,10 +3,6 @@ class Cat implements Animal {
         System.out.println("Meow, meow");
     }
     public void run() {
-<<<<<<< HEAD
         System.out.println("The cat is swimming");
-=======
-        System.out.println("Cat is sleeping");
->>>>>>> fd8595c2dfc41acb8f15e4579039118a17b17ba1
     }
 }
